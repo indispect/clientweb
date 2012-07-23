54ecf83d8780fad88632c7fe1ed6cf797bddb59f
indispect <amit.ashkenazi77@gmail.com>
2012-07-23 22:36:15 +0300

    init indi clientweb

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,23 @@
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.
+// You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
+
+window.fbAsyncInit = function() {
+  FB.init({
+    appId      : '341812065894310', // App ID
+    channelUrl : '//77.127.91.230:3000/channel.html', // Channel File
+    status     : true, // check login status
+    cookie     : true, // enable cookies to allow the server to access the session
+    xfbml      : true  // parse XFBML
+  });
+  // Additional initialization code here
+};
+// Load the SDK Asynchronously
+(function(d){
+   var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+   if (d.getElementById(id)) {return;}
+   js = d.createElement('script'); js.id = id; js.async = true;
+   js.src = "//connect.facebook.net/en_US/all.js";
+   ref.parentNode.insertBefore(js, ref);
+ }(document));
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Clientweb::Application.config.secret_token = '96a8cc02040c767cae7d0a5b18a37a49661215047796ba33a88ce649a50e2d2f5fe14e16f9d43cd24bd55e861af985b717db93ffae2f67361a4b81aff7b532b9'
