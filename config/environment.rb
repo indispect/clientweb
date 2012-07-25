266c107c15c3cbbc3be96d19734dc79e1fce82ff
indispect <amit.ashkenazi77@gmail.com>
2012-07-25 21:15:42 +0300

    fix

--- config/environment.rb
+++ config/environment.rb
@@ -3,8 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Clientweb::Application.initialize!
-
-config.action_dispatch.session = {
-  :key    => '_app_session',
-  :secret => '0x8cfvauh739op342en71hlkawq3x22...'
-}
