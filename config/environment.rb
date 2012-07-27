c44dde60c76985eb00fc7f26a62a754dae637a65
indispect <amit.ashkenazi77@gmail.com>
2012-07-27 18:13:54 +0300

    fb login

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,10 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
-
+Clientweb::Application.configure do
+  config.action_dispatch.session = {
+    :key    => '_app_session',
+    :secret => '0x0dkfj3927dkc7djdh36rkckdfzsg...'
+  }
+end
 # Initialize the rails application
 Clientweb::Application.initialize!
